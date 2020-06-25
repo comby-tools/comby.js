3f2d99cdec3562efb28c502bd5ce3d6c4fcee717
Rijnard van Tonder <rvantonder@gmail.com>
2020-06-25 16:38:29 -0700

    server using express

--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
-var url = require('url');
-var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
+var url = require('url');
 const { match, rewrite } = require("../js/comby.js")
 
-//create a server object:
-http.createServer(function (req, res) {
-  var parsed_url = url.parse(req.url, true);
-  var q = parsed_url.query;
-  var pathname = parsed_url.pathname
-  res.writeHead(200);
-  console.log('request ', req.url);
-  console.log('pathname ', pathname);
-  if (pathname == "/rewrite") {
-      console.log(q.data + " " + q.match_template + " " + q.rewrite_template);
-      var result = rewrite(q.data, q.match_template, q.rewrite_template);
-      res.write(result);
-  }
-  res.end(); //end the response
-}).listen(3289);
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+
+app.post('/rewrite', function (req, res) {
+   console.log("Got a POST request for /rewrite");
+   console.log('Body:', req.body);
+   console.log(req.body.data + " " + req.body.match_template + " " + req.body.rewrite_template);
+   var result = rewrite(req.body.data, req.body.match_template, req.body.rewrite_template);
+   res.send(result);
+})
+
+var server = app.listen(3289, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
