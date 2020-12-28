611305d32f34cea0efe12f5dfcd96d53811a39fa
Rijnard van Tonder <rvantonder@gmail.com>
2020-12-27 18:33:17 -0700

    hardcoded language select

--- src/comby.ml
+++ src/comby.ml
@@ -2,11 +2,16 @@ open Js_of_ocaml
 
 open Comby_js
 
+let select_matcher extension : (module Matchers.Matcher) =
+  match Matchers.Omega.select_with_extension extension with
+  | None -> (module Matchers.Omega.Go)
+  | Some m -> m
+
 let match_ : Js.js_string Js.t -> Js.js_string Js.t -> Js.js_string Js.t -> Js.js_string Js.t =
   fun source match_template rule ->
-  let module M = Matchers.Omega.Go in
   let source = Js.to_string source in
   let match_template = Js.to_string match_template in
+  let (module M) = select_matcher ".go" in
   let matches = M.all ?configuration:None ~rule:(Js.to_string rule) ~source ~template:match_template in
   let matches = Pipeline.update_line_col source matches in
   let result = Yojson.Safe.to_string (`List (List.map Match.to_yojson matches)) in
@@ -14,7 +19,7 @@ let match_ : Js.js_string Js.t -> Js.js_string Js.t -> Js.js_string Js.t -> Js.j
 
 let rewrite : Js.js_string Js.t -> Js.js_string Js.t -> Js.js_string Js.t -> Js.js_string Js.t -> Js.js_string Js.t =
   fun source match_template rewrite_template rule ->
-  let module M = Matchers.Omega.Go in
+  let (module M) = select_matcher ".go" in
   let source = Js.to_string source in
   let match_template = Js.to_string match_template in
   let rewrite_template = Js.to_string rewrite_template in
